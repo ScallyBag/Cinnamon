03ec6946d7303f29a778ca9a79c0b7114071169e
gekomad <giuseppe.cannella@gmail.com>
2021-03-25 21:50:29 +0100

    1

--- src/Search.cpp
+++ src/Search.cpp
@@ -21,7 +21,6 @@
 #include "SearchManager.h"
 #include "db/bitbase/kpk.h"
 
-Endgame *Search::endgame;
 bool volatile Search::runningThread;
 high_resolution_clock::time_point Search::startTime;
 using namespace _bitbase;
@@ -72,7 +71,6 @@ void Search::aspirationWindow(const int depth, const int valWin) {
 }
 
 Search::Search() : ponder(false), nullSearch(false) {
-    endgame = &Endgame::getInstance();
     DEBUG(eval.lazyEvalCuts = cumulativeMovesCount = totGen = 0)
 
 }
@@ -726,7 +724,7 @@ int Search::search(const int depth, int alpha, const int beta, _TpvLine *pline, 
     if (wdl != INT_MAX) return wdl;
 #endif
     if ((N_PIECE == 5 || N_PIECE == 4) && depth > 2 && depth != mainDepth) {
-        auto v = endgame->getEndgameValue<side>(chessboard, N_PIECE);
+        auto v = Endgame::getEndgameValue<side>(chessboard, N_PIECE);
         if (v != INT_MAX) {
             return v;
         }
--- src/db/Endgame.h
+++ src/db/Endgame.h
@@ -48,13 +48,12 @@ KNPK
 KNPKB
 KPKP
 */
-class Endgame : public Singleton<Endgame> {
-    friend class Singleton<Endgame>;
+class Endgame {
 
 public:
 
     template<int side>
-    int getEndgameValue(const _Tchessboard &chessboard, const int nPieces) {
+    static int getEndgameValue(const _Tchessboard &chessboard, const int nPieces) {
         ASSERT(nPieces != 999);
         ASSERT_RANGE(side, 0, 1);
         const auto posKingBlack = BITScanForward(chessboard[KING_BLACK]);
@@ -133,12 +132,12 @@ public:
     }
 
 private:
-    // Endgame();
+    Endgame();
 
-    const int DistanceBonus[8] = {0, 0, 100, 80, 60, 40, 20, 10};    //TODO stockfish
-    const int VALUE_KNOWN_WIN = 15000;    //TODO stockfish
-    const int penaltyKRKN[8] = {0, 10, 14, 20, 30, 42, 58, 80};    //TODO stockfish
-    const int KBNKMateTable[64] = {    //TODO stockfish
+    static constexpr uchar DistanceBonus[8] = {0, 0, 100, 80, 60, 40, 20, 10};    //TODO stockfish
+    static constexpr int VALUE_KNOWN_WIN = 15000;    //TODO stockfish
+    static constexpr int penaltyKRKN[8] = {0, 10, 14, 20, 30, 42, 58, 80};    //TODO stockfish
+    static constexpr int KBNKMateTable[64] = {    //TODO stockfish
             200, 190, 180, 170, 170, 180, 190, 200,
             190, 180, 170, 160, 160, 170, 180, 190,
             180, 170, 155, 140, 140, 155, 170, 180,
@@ -148,31 +147,13 @@ private:
             190, 180, 170, 160, 160, 170, 180, 190,
             200, 190, 180, 170, 170, 180, 190, 200};
 
-    const int MateTable[64] = {    //TODO stockfish
+    static constexpr int MateTable[64] = {    //TODO stockfish
             100, 90, 80, 70, 70, 80, 90, 100, 90, 70, 60, 50, 50, 60, 70, 90, 80, 60, 40, 30, 30, 40, 60, 80, 70, 50,
             30, 20, 20, 30, 50, 70, 70, 50, 30, 20, 20, 30, 50, 70, 80, 60, 40, 30, 30, 40, 60, 80, 90, 70, 60, 50, 50,
-            60, 70, 90, 100, 90, 80, 70, 70, 80, 90, 100,};
-
+            60, 70, 90, 100, 90, 80, 70, 70, 80, 90, 100};
 
 //    template<int loserSide>
-//    int KRKP(int tempo, int winnerKingPos, int loserKingPos, int rookPos, int pawnPos) {
-//#ifdef DEBUG_MODE
-//        std::unordered_map<int, int> pieces1;
-//        std::unordered_map<int, int> pieces2;
-//
-//        pieces1[KING_BLACK] = 1;
-//        pieces1[KING_WHITE] = 1;
-//        pieces1[ROOK_BLACK] = 1;
-//        pieces1[PAWN_WHITE] = 1;
-//
-//        pieces2[KING_BLACK] = 1;
-//        pieces2[KING_WHITE] = 1;
-//        pieces2[ROOK_WHITE] = 1;
-//        pieces2[PAWN_BLACK] = 1;
-//
-//
-//#endif
-//
+//    static int KRKP(const int tempo,const int winnerKingPos,const int loserKingPos,const int rookPos,const int pawnPos) {
 //        // If the stronger side's king is in front of the pawn, it's a win
 //        if (FILE_AT[winnerKingPos] == FILE_AT[pawnPos]) {
 //            if (loserSide == BLACK && winnerKingPos < pawnPos) {
@@ -203,126 +184,28 @@ private:
 //        }
 //    }
 
-//    int KRKQ(int winnerKingPos, int loserKingPos) {
-//
-//#ifdef DEBUG_MODE
-//        std::unordered_map<int, int> pieces1;
-//        std::unordered_map<int, int> pieces2;
-//
-//        pieces1[KING_BLACK] = 1;
-//        pieces1[KING_WHITE] = 1;
-//        pieces1[QUEEN_BLACK] = 1;
-//        pieces1[ROOK_WHITE] = 1;
-//
-//        pieces2[KING_BLACK] = 1;
-//        pieces2[KING_WHITE] = 1;
-//        pieces2[QUEEN_WHITE] = 1;
-//        pieces2[ROOK_BLACK] = 1;
-//
-//
-//        ASSERT_RANGE(winnerKingPos, 0, 63);
-//        ASSERT_RANGE(loserKingPos, 0, 63);
-//#endif
-//
+//    static int KRKQ(const int winnerKingPos,const int loserKingPos) {
 //        return VALUEQUEEN - VALUEROOK + MateTable[loserKingPos] + DistanceBonus[DISTANCE[winnerKingPos][loserKingPos]];
 //    }
-//
-//    int KRKB(int loserKingPos) {
-//
-//#ifdef DEBUG_MODE
-//        std::unordered_map<int, int> pieces1;
-//        std::unordered_map<int, int> pieces2;
-//
-//        pieces1[KING_BLACK] = 1;
-//        pieces1[KING_WHITE] = 1;
-//        pieces1[ROOK_BLACK] = 1;
-//        pieces1[BISHOP_WHITE] = 1;
-//
-//        pieces2[KING_BLACK] = 1;
-//        pieces2[KING_WHITE] = 1;
-//        pieces2[ROOK_WHITE] = 1;
-//        pieces2[BISHOP_BLACK] = 1;
-//
-//
-//        ASSERT_RANGE(loserKingPos, 0, 63);
-//#endif
-//
+
+//    static int KRKB(const int loserKingPos) {
 //        return MateTable[loserKingPos];
 //    }
-//
-//    int KRKN(int loserKingPos, int knightPos) {
-//
-//#ifdef DEBUG_MODE
-//        std::unordered_map<int, int> pieces1;
-//        std::unordered_map<int, int> pieces2;
-//
-//        pieces1[KING_BLACK] = 1;
-//        pieces1[KING_WHITE] = 1;
-//        pieces1[ROOK_BLACK] = 1;
-//        pieces1[KNIGHT_WHITE] = 1;
-//
-//        pieces2[KING_BLACK] = 1;
-//        pieces2[KING_WHITE] = 1;
-//        pieces2[ROOK_WHITE] = 1;
-//        pieces2[KNIGHT_BLACK] = 1;
-//
-//
-//        ASSERT_RANGE(loserKingPos, 0, 63);
-//#endif
-//
+
+//    static int KRKN(const int loserKingPos,const int knightPos) {
 //        return MateTable[loserKingPos] + penaltyKRKN[DISTANCE[loserKingPos][knightPos]];
 //    }
 //
-    int KQKP(int loserSide, int winnerKingPos, int loserKingPos, int pawnPos) {
-#ifdef DEBUG_MODE
-        std::unordered_map<int, int> pieces1;
-        std::unordered_map<int, int> pieces2;
-
-        pieces1[KING_BLACK] = 1;
-        pieces1[KING_WHITE] = 1;
-        pieces1[QUEEN_BLACK] = 1;
-        pieces1[PAWN_WHITE] = 1;
-
-        pieces2[KING_BLACK] = 1;
-        pieces2[KING_WHITE] = 1;
-        pieces2[QUEEN_WHITE] = 1;
-        pieces2[PAWN_BLACK] = 1;
-
-
-#endif
-
+    static int KQKP(const int loserSide,const  int winnerKingPos,const  int loserKingPos,const  int pawnPos) {
         int result = DistanceBonus[DISTANCE[winnerKingPos][loserKingPos]];
-        if ((DISTANCE[loserKingPos][pawnPos] != 1) || (RANK_AT[pawnPos] != (loserSide == WHITE ? 6
-                                                                                               : 1))
+        if ((DISTANCE[loserKingPos][pawnPos] != 1) || (RANK_AT[pawnPos] != (loserSide == WHITE ? 6 : 1))
             /*|| (0x5a5a5a5a5a5a5a5aULL & POW2(pawnPos))*/) {// 0x5a5a5a5a5a5a5a5aULL = FILE B D E F G
             result += VALUEQUEEN - VALUEPAWN;
         }
         return result;
     }
 
-//    int KBBKN(int winnerKingPos, int loserKingPos, int knightPos) {
-//
-//#ifdef DEBUG_MODE
-//
-//        std::unordered_map<int, int> pieces1;
-//        std::unordered_map<int, int> pieces2;
-//
-//        pieces1[KING_BLACK] = 1;
-//        pieces1[KING_WHITE] = 1;
-//        pieces1[BISHOP_BLACK] = 2;
-//        pieces1[KNIGHT_WHITE] = 1;
-//
-//        pieces2[KING_BLACK] = 1;
-//        pieces2[KING_WHITE] = 1;
-//        pieces2[BISHOP_WHITE] = 2;
-//        pieces2[KNIGHT_BLACK] = 1;
-//
-//
-//        ASSERT_RANGE(winnerKingPos, 0, 63);
-//        ASSERT_RANGE(knightPos, 0, 63);
-//        ASSERT_RANGE(loserKingPos, 0, 63);
-//#endif
-//
+//  static int KBBKN(const int winnerKingPos, const int loserKingPos, const int knightPos) {
 //        return VALUEBISHOP + DistanceBonus[DISTANCE[winnerKingPos][loserKingPos]] +
 //               (DISTANCE[loserKingPos][knightPos]) * 32;
 //        // Bonus for driving the defending king and knight apart
@@ -330,30 +213,9 @@ private:
 //        //result += Value((8 - popcount<Max15>(pos.attacks_from<KNIGHT>(nsq))) * 8);
 //    }
 //
-//    int KBNK(int winnerKingPos, int loserKingPos) {
-//
-//#ifdef DEBUG_MODE
-//        std::unordered_map<int, int> pieces1;
-//        std::unordered_map<int, int> pieces2;
-//
-//        pieces1[KING_BLACK] = 1;
-//        pieces1[KING_WHITE] = 1;
-//        pieces1[BISHOP_BLACK] = 1;
-//        pieces1[KNIGHT_BLACK] = 1;
-//
-//        pieces2[KING_BLACK] = 1;
-//        pieces2[KING_WHITE] = 1;
-//        pieces2[BISHOP_WHITE] = 1;
-//        pieces2[KNIGHT_WHITE] = 1;
-//
-//
-//        ASSERT_RANGE(winnerKingPos, 0, 63);
-//        ASSERT_RANGE(loserKingPos, 0, 63);
-//#endif
-//
+//    static int KBNK(const int winnerKingPos,const int loserKingPos) {
 //        return VALUE_KNOWN_WIN + DistanceBonus[DISTANCE[winnerKingPos][loserKingPos]] + KBNKMateTable[loserKingPos];
 //    }
-
 
 };
 
